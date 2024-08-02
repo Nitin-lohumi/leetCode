0ddeb401f0997804e7a46484686592336b8910b0
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2024-08-02 15:04:11 +0530

    Time: 58 ms (23.32%), Space: 49.1 MB (15.59%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    const obj = {
+        toBe(v){
+           if(val===v){
+               return true;
+           }
+           else{
+              throw new Error("Not Equal");
+           }
+        },
+      notToBe(v){
+       if(v!==val){
+        return true;
+       }
+       else{
+        throw new Error("Equal");
+       }
+        }
+    }
+    return obj;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
