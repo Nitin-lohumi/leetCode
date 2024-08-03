6812a805e433f180654a510dd7dc6c7b686f57f8
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2024-08-03 23:34:58 +0530

    Time: 69 ms (28.88%), Space: 50 MB (56.81%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -4,7 +4,6 @@
  */
 var compose = function (functions = []) {
     return function (x) {
-        // if (!functions.length) { return x }
         for (let i = functions.length - 1; i >= 0; i--) {
             x = functions[i](x);
         }
