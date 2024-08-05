e61740027dd2c5ed9193e6cfc7daf460fa0592cc
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2024-08-05 13:13:04 +0530

    Time: 49 ms (74.77%), Space: 48.1 MB (96.44%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let call = 0;
+    let cl;
+    return function(...args){
+     if(!call){
+        cl = fn(...args);
+        call = 1;
+        return cl;
+     }else{
+        return undefined;
+     }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
